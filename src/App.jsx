66aaeaccf7bfe19730eaf0e3bca314d82d625981
Rafael Simas <rafael.simas@al.resilia.com.br>
2025-01-01 00:17:27 -0300

    Banner mobile e desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Menu from "./Components/Menu/Menu"
-
+import BannerHome from "./Components/BannerHome/BannerHome"
 export default function App() {
-  return <Menu />
+  return (
+    <>
+      <Menu />
+      <BannerHome />
+    </>
+  )
 }
